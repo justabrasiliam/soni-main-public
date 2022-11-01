34ab18c6d134cdf5beb9c0290c116edd6b3ef329
Just a Brazilian <100686396+justabrasiliam@users.noreply.github.com>
2022-11-01 17:11:22 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -213,6 +213,10 @@ class StoryMenuState extends MusicBeatState
 		changeWeek();
 		changeDifficulty();
 
+		#if android
+                addVirtualPad(LEFT_FULL, A_B);
+                #end
+		
 		super.create();
 	}
 
